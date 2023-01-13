d5755d78a3cb3e94dcc3c5a8d3c58e2710896fde
Brian Mungai <bryanmungai254@gmail.com>
2023-01-13 22:03:02 +0300

    handle form submission

--- main.go
+++ main.go
@@ -27,13 +27,37 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
 }
 
 
+//formHandler func
+func formHandler(w http.ResponseWriter, r *http.Request){
+	//check for error
+	//ParseForm pass the raw query and update value to r.PostForm and r.Form
+	if err := r.ParseForm(); err != nil{
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+	}
+
+	//if no error
+	fmt.Fprintf(w, "Post request successful")
+
+
+	//get name and address value using Form value
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name =%s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+
 func main(){
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	//Handle - accept path and fileserver
+	//display index.html file
 	http.Handle("/", fileServer)
 
 	http.HandleFunc("/hello", helloHandler)
+
+	http.HandleFunc("/form", f)
 
 
 	fmt.Printf("starting server at port 8080\n")
